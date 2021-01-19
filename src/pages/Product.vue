6ba9d1cbbbf780e3f0d3b3491f2db2cabb5b23d6
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-19 13:57:00 +0300

    added a slider

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -2,9 +2,23 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-          <img :src="product.img" :alt="product.title">
-        <h1 class="title">{{ product.title }}</h1>
-        <p> {{ product.descr }}</p>
+        <div class="product__wrapper">
+          <div class="product__slider">
+            <carousel :PerPage = "1"
+            :paginationEnable = "true"
+            paginationColor = "#b3b3b3"
+            paginationActiveColor = "#494ce8"
+            >
+                <slide v-for="(slide, index) in product.gallery" :key="index">
+                   <img :src="slide.img" :alt="slide.name" class="img">
+                </slide>
+            </carousel>
+          </div>
+          <div class="product__content">
+             <h1 class="title">{{ product.title }}</h1>
+             <p> {{ product.descr }}</p>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -23,3 +37,21 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+.product__wrapper{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+}
+.product__content,
+.product__slider{
+max-width: 48%;
+text-align: center;
+}
+.VueCarousel-inner{
+visibility: visible!important;
+flex-basis: 100%!important;
+width: 100%!important;
+}
+</style>
